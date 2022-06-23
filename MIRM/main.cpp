b939c77d0f71527d780e62fbfde2740a12e2bd4f
Administrator <Administrator@PC-hw-01>
2022-06-23 15:26:29 +0900

    0623 필터링추가

--- MIRM/main.cpp
+++ MIRM/main.cpp
@@ -12,20 +12,51 @@ void __fastcall HookedPe(UObject* obj, UFunction* func, void* pParams) {
 				auto gWorld = (UWorld*)*(DWORD64*)(Offsets::Base + Offsets::gWorldOffset);
 				auto MyVec = gWorld->OwningGameInstance->LocalPlayers[0]->PlayerController->Pawn->RootComponent->RelativeLocation;
 				Utils::LogConsole("현재 내 위치 : %f %f %f\n", MyVec.X, MyVec.Y, MyVec.Z);
+				Utils::LogConsole("OwningGameInstance : 0x%llX \n", gWorld->OwningGameInstance);
 			}
 
 			if (PrintFunc.find("Anim") == std::string::npos &&
 				PrintFunc.find("Tick") == std::string::npos &&
 				PrintFunc.find("Blueprint") == std::string::npos &&
-				PrintFunc.find("Prefab") == std::string::npos &&
+				PrintFunc.find("ChangeCflag") == std::string::npos &&
 				PrintFunc.find("NavMesh") == std::string::npos &&
 				PrintFunc.find("MirImage") == std::string::npos &&
 				PrintFunc.find("Opacity") == std::string::npos &&
-				PrintFunc.find("UMG") == std::string::npos &&
+				PrintFunc.find("OnMouseMove") == std::string::npos &&
+				PrintFunc.find("Name_Tag") == std::string::npos &&
+				PrintFunc.find("OnMouseEnter") == std::string::npos &&
+				PrintFunc.find("OnMouseLeave") == std::string::npos &&
+				PrintFunc.find("OnInit") == std::string::npos &&
+				PrintFunc.find("ChatSlot") == std::string::npos &&
+				PrintFunc.find("GamePad") == std::string::npos &&
 				PrintFunc.find("IsInOpenDungeon") == std::string::npos &&
 				PrintFunc.find("SetIntensity") == std::string::npos &&
 				PrintFunc.find("OnSetAlarmState") == std::string::npos &&
-				PrintFunc.find("TimerEvent") == std::string::npos 
+				PrintFunc.find("TimerEvent") == std::string::npos &&
+				PrintFunc.find("CheckInArea") == std::string::npos &&
+				PrintFunc.find("OnShowHPBar") == std::string::npos &&
+				PrintFunc.find("GetTextCurPos") == std::string::npos &&
+				PrintFunc.find("OnSetLookAt") == std::string::npos &&
+				PrintFunc.find("ProcessSleepMode") == std::string::npos &&
+				PrintFunc.find("Gamepad") == std::string::npos &&
+				PrintFunc.find("InputAxis") == std::string::npos &&
+				PrintFunc.find("CheckValid") == std::string::npos &&
+				PrintFunc.find("Position") == std::string::npos &&
+				PrintFunc.find("Visible") == std::string::npos &&
+				PrintFunc.find("Protocol") == std::string::npos &&
+				PrintFunc.find("Receive") == std::string::npos &&
+				PrintFunc.find("SetCollisionResponseToChannel") == std::string::npos &&
+				PrintFunc.find("OnPreSet") == std::string::npos &&
+				PrintFunc.find("Volume") == std::string::npos &&
+				PrintFunc.find("Icon") == std::string::npos &&
+				PrintFunc.find("Construct") == std::string::npos &&
+				PrintFunc.find("CheckDrawType") == std::string::npos &&
+				PrintFunc.find("NamTag") == std::string::npos &&
+				PrintFunc.find("OnSet") == std::string::npos &&
+				PrintFunc.find("CheckDrawType") == std::string::npos &&
+				PrintFunc.find("ReceiveBeginPlay") == std::string::npos &&
+				PrintFunc.find("NamTag") == std::string::npos &&
+				PrintFunc.find("OnSetText") == std::string::npos
 				) 
 			{
 				Utils::LogConsole("[ 0x%llX ]\n함수이름 : %s\n오브이름 : %s\n", obj, PrintFunc.c_str(), obj->GetFullName().c_str());
